a3e994f293ca553bec4ae595f4fe43bbce472578
Putra-trilaksana <dhitabintangputrat6983@gmail.com>
2022-10-11 18:14:56 +0700

    Tambahan Testimoni

---
+++ src/components/Testimoni.vue
@@ -0,0 +1,22 @@
+<script>
+import CardTestimoni from './CardTestimoni.vue';
+export default {
+    components: { CardTestimoni }
+}
+</script>
+
+<template>
+    <div class="container">
+        <h3 class="d-flex col-12 justify-content-center">TESTIMONI PELANGGAN</h3>
+        <div class="d-flex ">
+            <CardTestimoni />
+            <CardTestimoni />
+            <CardTestimoni />
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
